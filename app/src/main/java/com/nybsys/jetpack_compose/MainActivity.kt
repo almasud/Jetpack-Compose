914afa7c3408eb359bd2384e63da2bbaa9bacbfa
almasud <dev.almasud@gmail.com>
2022-11-21 18:26:33 +0600

    Commit for composable text.

--- app/src/main/java/com/nybsys/jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/nybsys/jetpack_compose/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.nybsys.jetpack_compose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.nybsys.jetpack_compose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +24,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Title("Android")
                 }
             }
         }
@@ -30,14 +33,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Title(name: String) {
+    Surface(modifier = Modifier.wrapContentSize()) {
+        val context = LocalContext.current
+        Text(
+            text = "Hello $name!",
+            fontSize = 22.sp,
+            color = colorResource(id = R.color.purple_700),
+            fontFamily = FontFamily.Cursive,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.clickable {
+                Toast.makeText(context, "Title clicked!", Toast.LENGTH_SHORT).show()
+            },
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
-        Greeting("Android")
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Title("Android")
+        }
     }
 }
