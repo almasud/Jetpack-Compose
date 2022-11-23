5fd627c6132aed95f3fa58bc42aa84914853fcf3
almasud <dev.almasud@gmail.com>
2022-11-23 13:00:46 +0600

    Compose in Fragment.

--- app/src/main/java/com/nybsys/jetpack_compose/ui/user/UserFragment.kt
+++ app/src/main/java/com/nybsys/jetpack_compose/ui/user/UserFragment.kt
@@ -1,8 +1,11 @@
 package com.nybsys.jetpack_compose.ui.user
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
 import com.nybsys.jetpack_compose.R
@@ -11,16 +14,32 @@ import com.nybsys.jetpack_compose.databinding.FragmentUserBinding
 /**
  * A simple [Fragment] subclass.
  */
-class UserFragment : Fragment(R.layout.fragment_user) {
-    private lateinit var binding: FragmentUserBinding
+// Compose in XML
+//class UserFragment : Fragment(R.layout.fragment_user) {
+class UserFragment : Fragment() {
+    // Compose in XML
+//    private lateinit var binding: FragmentUserBinding
 
     // Compose in XML
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding = FragmentUserBinding.bind(view)
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        binding = FragmentUserBinding.bind(view)
+//
+//        binding.composeView.setContent {
+//            UserListScreen()
+//        }
+//    }
 
-        binding.composeView.setContent {
-            UserListScreen()
+    // Compose in Fragment
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                UserListScreen()
+            }
         }
     }
 }
